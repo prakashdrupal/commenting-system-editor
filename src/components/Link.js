6e62dacdb7cf655bbbc064a98c62731c6d30fd41
Tej Prakash Prasad <8806463+prakashdrupal@users.noreply.github.com>
2021-09-07 16:16:31 +0530

    remove code

--- src/components/Link.js
+++
@@ -1,9 +0,0 @@
-import "./Link.css";
-
-export default function Link({ element, attributes, children }) {
-  return (
-    <a href={element.url} {...attributes} className={"link"}>
-      {children}
-    </a>
-  );
-}
